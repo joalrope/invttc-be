a969c60a467a538b04e66a80a4592b0f7191d8f5
joalrope <joalrope@gmail.com>
2022-02-15 08:01:25 -0400

    Sale model refact

--- models/Sale.js
+++ models/Sale.js
@@ -19,8 +19,17 @@ const SaleSchema = Schema(
       code: {
         type: String,
         required: true,
+        unique: true,
       },
       name: {
+        type: String,
+        required: true,
+      },
+      address: {
+        type: String,
+        required: true,
+      },
+      phone: {
         type: String,
       },
     },
